8e918fa4d4ca3312a610e337614112bef07e7e98
Prashantdwivedi12 <43915948+Prashantdwivedi12@users.noreply.github.com>
2020-05-08 03:57:35 +0530

    Create F_C.java

---
+++ F_C.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int S = sc.nextInt(); // start point of Fahrenheit
+		int E = sc.nextInt(); // end point of Fahrenheit
+		int W = sc.nextInt();// jump or gap
+		int c;
+		while (S<=E) {
+			c = 5 * (S-32)/9;
+			System.out.print(S);
+			System.out.print(" ");
+			System.out.println(c);
+			S=S+W;
+		}
+		
+		return;
+	
+		
+	}
+}
